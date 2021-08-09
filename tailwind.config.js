20104ad6bb96dfa920af5a721dea544b37c5d3d9
ken505 <68226398+ken505@users.noreply.github.com>
2021-08-09 12:01:59 +0900

    ✨ Add jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  mode: "jit",
   purge: ["./index.html"],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
   },
@@ -8,4 +9,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
